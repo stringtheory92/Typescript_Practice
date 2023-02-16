d34b84a34db5e46068f8c0ace2e78dd81d763593
Adam Kabak <adamkabak@yahoo.com>
2023-02-16 08:23:34 -0500

    first commit

---
+++ dist/index.js
@@ -0,0 +1,14 @@
+"use strict";
+class Account {
+    constructor(id, owner, balance) {
+        this.id = id;
+        this.owner = owner;
+        this.balance = balance;
+    }
+    deposit(amount) {
+        if (amount <= 0)
+            throw new Error("Invalid amount");
+        this.balance += amount;
+    }
+}
+//# sourceMappingURL=index.js.map
