69a9e8bf15fe8cc855068b4fe303ff435a759dd4
Adam Kabak <adamkabak@yahoo.com>
2023-02-16 08:36:53 -0500

    account class

--- dist/index.js
+++ dist/index.js
@@ -3,12 +3,26 @@ class Account {
     constructor(id, owner, balance) {
         this.id = id;
         this.owner = owner;
-        this.balance = balance;
+        this._balance = balance;
     }
     deposit(amount) {
         if (amount <= 0)
             throw new Error("Invalid amount");
-        this.balance += amount;
+        this._balance += amount;
+    }
+    calculateTax(bill) {
+        return bill * 0.11;
+    }
+    getBalance() {
+        return this._balance;
+    }
+    pay(bill) {
+        this._balance -= bill + this.calculateTax(bill);
+        return this._balance;
     }
 }
+let account = new Account(1, "Adam", 0);
+account.deposit(1000);
+console.log(account.getBalance());
+console.log("pay $100: ", account.pay(100));
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,36 @@
 class Account {
-  id: number;
+  readonly id: number;
   owner: string;
-  balance: number;
+  private _balance: number;
+  nickname?: string;
 
   constructor(id: number, owner: string, balance: number) {
     this.id = id;
     this.owner = owner;
-    this.balance = balance;
+    this._balance = balance;
   }
 
   deposit(amount: number): void {
     if (amount <= 0) throw new Error("Invalid amount");
-    this.balance += amount;
+
+    this._balance += amount;
+  }
+
+  private calculateTax(bill: number) {
+    return bill * 0.11;
+  }
+
+  getBalance(): number {
+    return this._balance;
+  }
+
+  pay(bill: number): number {
+    this._balance -= bill + this.calculateTax(bill);
+    return this._balance;
   }
 }
+
+let account = new Account(1, "Adam", 0);
+account.deposit(1000);
+console.log(account.getBalance());
+console.log("pay $100 + tax: ", account.pay(100));
